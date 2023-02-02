bfb7d6bd1acba5a1ed5b2bb15727aa76f14a3ba8
alissoncaicedo <manucaicedo1205@gmail.com>
2023-02-02 12:13:15 -0500

    se crea  metodos

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Empleado {
-}
+    //ATRIBUTOS
+    String nombre;
+
+    //CONSTRUCTORES
+
+    public Empleado() {
+    }
+
+    //METODO ESPECIAL
+    //la licuadora
+    // es el tipo de retorno de la funcionn
+    public String saludar(String nombresito){
+        String saludo= "Hola " + nombresito;
+        return saludo;
+
+
+    }
+    public int sumar(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+
+
